4fabcbc3effeba552d8362a77326a0548e8ddc8a
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-03-23 18:04:43 +0200

    Added factsheet database endpoint

---
+++ src/bise/content/browser/api.py
@@ -0,0 +1,46 @@
+""" plone.restapi extensions and endpoints
+"""
+
+from bise.content.interfaces import IBiseFactsheetDatabase
+from plone import api
+from plone.restapi.interfaces import IExpandableElement
+from plone.restapi.interfaces import ISerializeToJson
+from plone.restapi.services import Service
+from zope.component import adapter
+from zope.component import getMultiAdapter
+from zope.interface import implementer
+from zope.interface import Interface
+
+
+@implementer(IExpandableElement)
+@adapter(IBiseFactsheetDatabase, Interface)
+class FactsheetDatabaseListing(object):
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self, expand=False):
+        # we ignore expand here, because of specific interface
+        result = {}
+        batch = api.content.find(context=self.context,
+                                 portal_type="bise_factsheet_section",
+                                 sort_on="getObjPositionInParent",
+                                 sort_order="ascending",
+                                 )
+        items = [
+            getMultiAdapter((brain.getObject(), self.request),
+                            ISerializeToJson)()
+
+            for brain in batch
+        ]
+
+        result["factsheet-database-listing"] = items
+
+        return result
+
+
+# class FactsheetDatabaseListing(Service):
+#     def reply(self):
+#         data = ConnectorData(self.context, self.request)
+#
+#         return data(expand=True)["connector-data"]
--- src/bise/content/interfaces.py
+++ src/bise/content/interfaces.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
 class IBiseContentLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IBiseFactsheetDatabase(Interface):
+    """ A marker interface for content serving as factsheets "database"
+    """
