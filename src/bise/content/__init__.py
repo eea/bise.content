69626c5525d2317784afecc41739a46fb4331c78
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-11-25 09:12:22 +0200

    Bump up default batching size

--- src/bise/content/__init__.py
+++ src/bise/content/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
+from plone.restapi import batching
 from zope.i18nmessageid import MessageFactory
 
 
 _ = MessageFactory('bise.content')
+
+
+batching.DEFAULT_BATCH_SIZE = 1000
