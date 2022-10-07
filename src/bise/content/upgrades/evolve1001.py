8442d3eb7176da7edeebc8c5bbb5194f161362c7
Miu Razvan <miu.razvan28@gmail.com>
2022-10-07 12:44:29 +0300

    Upgrade step: convert all CompositePage to Document

---
+++ src/bise/content/upgrades/evolve1001.py
@@ -0,0 +1,16 @@
+""" Upgrade to version 1001
+"""
+# pylint: disable=line-too-long
+from Products.CMFCore.utils import getToolByName
+
+
+def migrate_composite_page(context):
+    """ Fix Indicator schema """
+    ctool = getToolByName(context, "portal_catalog")
+    portal_type = "CompositePage"
+    brains = ctool.unrestrictedSearchResults(portal_type=portal_type)
+
+    for brain in brains:
+        doc = brain.getObject()
+        setattr(doc, 'portal_type', 'Document')
+        doc.reindexObject()
